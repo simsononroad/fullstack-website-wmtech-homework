77697e41e782e81cbc1160f894b4615184572cc0
simsononroad <d-github@kopaszhegy.hu>
2024-12-04 19:25:16 +0100

    login_fix

--- app.py
+++ app.py
@@ -3,19 +3,33 @@ from flask import Flask, request, render_template, redirect, url_for, flash, ses
 import hashlib
 import sqlite3
 
+# Felhasználó hazzáadásához szükséges lépések:
+#   1. Nézze meg egy másik file-ban hogy a tesztelni kívánt jelszónak mi a kódja.
+#   2. hozzon itt létre egy "password_in_hash_x" változót
+#   3. A try-on belül hozzon létre egy parancsoot ami beteszi a helyére a felhasználóneve és jelszót
+
+password_in_hash_1 = "4c00da99010ff9be00046e7854e2bd5cfca292ae9c629421ae89b08a5370e91c"
+password_in_hash_2 = "c52c5501c5ddd107202a95915b4466f98773b611a09cf8a283ebbe2760f6236a"
+password_in_hash_3 = "f854b2aeb789d616b26acff4df9ff7741d09d5f7a63add238201880e5b10bf2b"
 con = sqlite3.connect("login.db")
 cur = con.cursor()
 try:
     cur.execute("CREATE TABLE login(id INT PRIMARY KEY ,name, password)")
+    ins = cur.execute(f"insert into login (name, password) values ('admin', '{password_in_hash_1}')")
+    con.commit()
+    ins = cur.execute(f"insert into login (name, password) values ('teszt', '{password_in_hash_2}')")
+    con.commit()
+    ins = cur.execute(f"insert into login (name, password) values ('ok', '{password_in_hash_3}')")
+    con.commit()
 except:
     pass
-ins = cur.execute(f"insert into login (name, password) values ('admin', '{password_hash}')")
-con.commit()
 
-cur.execute("SELECT name FROM login;")
-namedb = cur.fetchone()
-cur.execute("SELECT password FROM login;")
-passworddb = cur.fetchone()
+
+
+
+
+
+
 
 
 app = Flask(__name__)
@@ -24,8 +38,7 @@ app.secret_key = "szupertitkoskulcs"  # Ezt cseréld le egy erősebb kulcsra!
 
 
 # Előre meghatározott felhasználónév és jelszó
-VALID_USERNAME = namedb
-VALID_PASSWORD = passworddb
+
 
 # Főoldal (index)
 @app.route("/")
@@ -35,17 +48,35 @@ def index():
 # Bejelentkezés
 @app.route("/login", methods=["POST"])
 def login():
-    
-
+    con = sqlite3.connect("login.db")
+    cur = con.cursor()
+    username_in_html = request.form['username']
+    password_in_html = request.form['password']
+    password_hash = hashlib.sha256(password_in_html.encode("UTF-8")).hexdigest()
+    username_in_html_felesleggel = f"('{username_in_html}',)"
+    password_hash_felesleggel = f"('{password_in_html}',)"
+    print("================================")
+    print(username_in_html_felesleggel)
+    print(password_hash_felesleggel)
+    print("================================")
     # Hitelesítés az előre megadott adatokkal
-    password_hash = hashlib.sha256(passwordh.encode("UTF-8")).hexdigest()
-    if usernameh == VALID_USERNAME and password_hash == VALID_PASSWORD:
-        session["user"] = username
-        flash("Sikeres bejelentkezés!", "success")
-        return redirect(url_for("dashboard"))
-    else:
+    
+    cur.execute(f"SELECT name, password FROM login WHERE name = \"{username_in_html}\"")
+    login_in_db = cur.fetchall()
+    print(login_in_db)
+    
+    if len(login_in_db) == 0:
         flash("Helytelen felhasználónév vagy jelszó. \nFelhasználónév: admin || Jelszó:titkos", "error")
         return redirect(url_for("index"))
+    if login_in_db[0][1] != password_hash:
+        flash("Helytelen felhasználónév vagy jelszó. \nFelhasználónév: admin || Jelszó:titkos", "error")
+        return redirect(url_for("index"))
+    session["user"] = username_in_html
+    flash("Sikeres bejelentkezés!", "success")
+    return redirect(url_for("dashboard"))
+    
+    
+    
 
 # Dashboard (csak bejelentkezett felhasználóknak)
 @app.route("/dashboard")
